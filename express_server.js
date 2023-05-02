c89e776639683f5f47580d691e7b8b1f947084da
AliyaJK <kanani.aliya@gmail.com>
2023-05-02 18:34:27 +0000

    add login form to header partial and post route for login to express_server.js

--- express_server.js
+++ express_server.js
@@ -89,3 +89,9 @@ app.post('/urls/:id', (req, res) => {
   urlDatabase[shortURL] = longURL;
   res.redirect('/urls');
 });
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect('/urls');
+});
